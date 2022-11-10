716cf1ef1c6335e737a00548082e180d982dd11e
ayberk mogol <ayberk.m@yandex.com>
2022-11-10 21:58:55 +0300

    Project init

---
+++ Movillium/Network/ApiManager.swift
@@ -0,0 +1,26 @@
+//
+//  ApiManager.swift
+//  Movillium
+//
+//  Created by Ayberk Mogol on 10.11.2022.
+//
+
+import Foundation
+
+class ApiManager {
+    
+    class func getApiResponse(urlPath: String, completionHandler: @escaping (Data?, Error?) -> Void ) {
+        let urlSession = URLSession.shared
+        guard let url = URL.init(string: urlPath) else { return }
+        
+        let task = urlSession.dataTask(with: url) { data, response, error in
+            
+            if error != nil {
+                completionHandler(nil, error)
+            } else {
+                completionHandler(data, nil)
+            }
+        }
+        task.resume()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Movillium' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Movillium
+    pod 'Alamofire'
+    pod 'SDWebImage'
+
+  target 'MovilliumTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MovilliumUITests' do
+    # Pods for testing
+  end
+
+end
